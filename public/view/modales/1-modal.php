752e33622b77ccb2138ba7239590f24bb694d8d9
Daniela Infantes <danielamij251@gmail.com>
2024-02-22 20:49:27 -0300

    Modal de sección Branding y diseño

--- public/view/modales/1-modal.php
+++ public/view/modales/1-modal.php
@@ -130,11 +130,12 @@
 .modal-main .modal-main-image figcaption {
     margin-top: 1rem;
     font-size: 24px;
+    text-transform: uppercase;
+
 }
 
 .modal-main .modal-main-image figcaption span {
     font-weight: bold;
-    text-transform: uppercase;
     text-decoration: underline;
 }
 
@@ -246,12 +247,6 @@
         </div>
     </div>
 </div>
-
-
-<!-- JS bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-</script>
 
 <script>
 const btnCerrar = document.querySelector('.btn-cerrar');
--- public/view/modales/4-modal.php
+++ public/view/modales/4-modal.php
@@ -1,3 +1,290 @@
-<section class="modal-branding-diseno">
-    <h1>Hola Branding y Diseño</h1>
-</section>
+<style>
+.modal-main-background {
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    z-index: 1050;
+    justify-content: center;
+    align-items: center;
+    display: none;
+}
+
+.modal-main {
+    background-color: rgb(177 13 98);
+    color: white;
+    padding: 2rem 3rem;
+    border-radius: 40px;
+    text-align: center;
+    position: relative;
+    width: 65vw;
+}
+
+.modal-main .modal-main-content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.modal-main .btn-cerrar {
+    position: absolute;
+    right: 25px;
+    top: 25px;
+    background-color: rgb(239 21 150);
+    width: 40px;
+    height: 40px;
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 3px solid white;
+
+    i {
+        font-size: x-large;
+    }
+
+    i:hover {
+        cursor: pointer;
+    }
+}
+
+.modal-main h3 {
+    font-size: 2vw;
+    font-weight: bold;
+    font-family: "DM Sans", sans-serif;
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+    font-weight: 1000;
+    font-style: normal;
+    margin-bottom: 1rem;
+}
+
+.modal-main p {
+    font-size: 2vw;
+    font-style: italic;
+    position: relative;
+}
+
+.modal-main .modal-main-form {
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+}
+
+
+.modal-main .form-inputs {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-main .form-inputs .form-group {
+    text-align: start;
+    width: 90%;
+}
+
+.modal-main .form-inputs .form-group input {
+    padding: 14px 24px;
+    border: none;
+    border-radius: 50px;
+}
+
+.modal-main .form-inputs .form-group input::placeholder {
+    color: rgb(77 76 76);
+    font-size: 20px;
+}
+
+.modal-main .form-inputs .form-group label {
+
+    text-transform: uppercase;
+    font-weight: 100;
+    letter-spacing: 3px;
+    padding-left: 12px;
+    margin-bottom: 8px;
+}
+
+.modal-main button {
+    font-size: 23px;
+    margin: 1rem auto;
+    padding: 15px 50px;
+    width: fit-content;
+    border: none;
+    color: white;
+    font-weight: 600;
+    border-radius: 50px;
+    background: rgb(25 135 232);
+}
+
+.modal-main button:hover {
+    cursor: pointer;
+}
+
+.modal-main .modal-main-image {
+    width: 100%;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 0;
+
+}
+
+.modal-main .modal-main-image img {
+    width: 60%;
+    height: 60%;
+    border-radius: 50px;
+}
+
+.modal-main .modal-main-image figcaption {
+    margin-top: 1rem;
+    font-size: 24px;
+    text-transform: uppercase;
+}
+
+.modal-main .modal-main-image figcaption span {
+    font-weight: bold;
+    text-decoration: underline;
+}
+
+@media (max-width: 1024px) {
+
+    .modal-main {
+        padding: 5rem 2rem 2rem;
+        width: 80vw;
+    }
+
+    .modal-main button {
+        width: 90%;
+    }
+}
+
+@media (max-width: 768px) {
+
+    .modal-main {
+        padding: 4rem 1rem 2rem;
+        width: 90vw;
+    }
+
+    .modal-main .modal-main-content {
+        grid-template-columns: 1fr;
+    }
+
+    .modal-main .modal-main-image {
+        display: none;
+    }
+
+    .modal-main .form-inputs {
+        gap: 1rem;
+    }
+
+    .modal-main h3 {
+        font-size: 6vw;
+        margin-top: 2rem
+    }
+
+    .modal-main button {
+        margin-top: 2rem;
+    }
+
+
+}
+
+@media (max-width: 425px) {
+
+    .modal-main h3 {
+        font-size: 6.5vw;
+        margin-top: 4rem;
+    }
+
+    .modal-main .form-inputs .form-group input {
+        padding: 12px 24px;
+    }
+
+    .modal-main .form-inputs {
+        gap: 0rem;
+    }
+
+    .modal-main .form-inputs .form-group label {
+        font-size: 14px;
+    }
+
+    .modal-main .form-inputs .form-group input::placeholder {
+        font-size: 16px;
+    }
+
+    .modal-main button {
+        padding: 10px 20px;
+        margin-top: 1rem;
+        font-size: 20px;
+    }
+}
+</style>
+
+<div class="modal-main-background">
+    <div class="modal-main">
+        <figure class="btn-cerrar">
+            <i class="fa solid fa-xmark fa-lg"></i>
+        </figure>
+        <!-- <h3>¡DISEÑA TU CAMINO HACIA EL ÉXITO!</h3> -->
+        <div class="modal-main-content">
+
+            <figure class="modal-main-image">
+                <h3>¡DISEÑA TU CAMINO HACIA EL ÉXITO!</h3>
+
+                <img src="./public/img/modal-img-4.jpeg" alt="modal diseno y desarrollo">
+                <figcaption>¡Primera asesoría <span>GRATIS!</span></figcaption>
+            </figure>
+
+            <form class="modal-main-form" action="../email/enviar-prueba.php" method="post">
+                <div class="form-inputs">
+                    <!--//Nombre//-->
+                    <div class="form-group">
+                        <label for="name">Nombre Completo</label>
+                        <input type="text" class="form-control" placeholder="Jimena Vásquez" name="name" id="name">
+                        <p class="err-msg"></p>
+                    </div>
+
+                    <!--//Correo//-->
+                    <div class="form-group">
+                        <label for="email">Correo Electrónico</label>
+                        <input type="text" class="form-control" placeholder="hola@sitioincreible.com.ar" name="email"
+                            id="email">
+                        <p class="err-msg"></p>
+                    </div>
+
+                    <!--//Telefono//-->
+                    <div class="form-group">
+                        <label for="phone">Teléfono</label>
+                        <input type="text" class="form-control" placeholder="987654321" name="phone" id="phone">
+                        <p class="err-msg"></p>
+                    </div>
+                </div>
+                <button type="submit" class="go-button" name="submit">¡Sé la diferencia!</button>
+            </form>
+
+
+        </div>
+    </div>
+</div>
+
+
+<script>
+const btnCerrar = document.querySelector('.btn-cerrar');
+const modalContainer = document.querySelector('.modal-main-background')
+
+
+document.addEventListener("DOMContentLoaded", mostrarModalDespuesDe5Segundos);
+
+// Función para mostrar el modal después de 1 segundos
+function mostrarModalDespuesDe5Segundos() {
+    setTimeout(() => {
+        modalContainer.style.display = 'flex';
+    }, 1000); // 1000 milisegundos = 1 segundos
+}
+
+// Agregar evento al botón de cerrar
+btnCerrar.addEventListener('click', () => {
+    console.log("clickeaste");
+    modalContainer.style.display = 'none';
+});
+</script>
